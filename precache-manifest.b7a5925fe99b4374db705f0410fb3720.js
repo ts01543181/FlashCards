5e6d3661853eb029a805f88be7fd37895e42651a
Joseph Cheng <joecheng950103@gmail.com>
2019-04-24 23:08:16 -0700

    Updates

---
+++ precache-manifest.b7a5925fe99b4374db705f0410fb3720.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "638cbeec08b08e539f19",
+    "url": "/FlashCards/static/js/runtime~main.8ea8ad21.js"
+  },
+  {
+    "revision": "ebb082b267cd9efe91a0",
+    "url": "/FlashCards/static/js/main.19766d5f.chunk.js"
+  },
+  {
+    "revision": "0f1085c9835b01acfa9e",
+    "url": "/FlashCards/static/js/2.cf283f27.chunk.js"
+  },
+  {
+    "revision": "ebb082b267cd9efe91a0",
+    "url": "/FlashCards/static/css/main.d0d242f2.chunk.css"
+  },
+  {
+    "revision": "de8ef5a01d62ffaa31eddbfbdd8d1851",
+    "url": "/FlashCards/index.html"
+  }
+];
