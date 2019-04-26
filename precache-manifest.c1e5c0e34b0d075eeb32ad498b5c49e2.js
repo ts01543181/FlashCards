7236cebcacaab5c9ed1c99f9a8b1b8638d8d557d
Joseph Cheng <joecheng950103@gmail.com>
2019-04-25 23:30:33 -0700

    Updates

---
+++ precache-manifest.c1e5c0e34b0d075eeb32ad498b5c49e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "638cbeec08b08e539f19",
+    "url": "/FlashCards/static/js/runtime~main.8ea8ad21.js"
+  },
+  {
+    "revision": "f77e48c16e89ce4b252b",
+    "url": "/FlashCards/static/js/main.8d5f69db.chunk.js"
+  },
+  {
+    "revision": "cc14111c5b48e66e90ce",
+    "url": "/FlashCards/static/js/2.24ddc1bb.chunk.js"
+  },
+  {
+    "revision": "f77e48c16e89ce4b252b",
+    "url": "/FlashCards/static/css/main.12121960.chunk.css"
+  },
+  {
+    "revision": "ca1ceac2125d981c43eaec9f78c9430d",
+    "url": "/FlashCards/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/FlashCards/precache-manifest.f3f27bf18d85a042302b16fbaba70a59.js"
+  "/FlashCards/precache-manifest.c1e5c0e34b0d075eeb32ad498b5c49e2.js"
 );
 
 workbox.clientsClaim();
